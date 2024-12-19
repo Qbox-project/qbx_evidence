57517c04596c557e11b22c62ce201512d5242bcf
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-12-18 20:49:09 -0500

    refactor: gsr

--- config/shared.lua
+++ config/shared.lua
@@ -4,7 +4,8 @@ return {
             label = locale('statuses.gsr'),
             threshold = 2,
             chance = 70,
-            duration = 900000, -- 15 minutes
+            duration = 900000, -- Time in ms that the state stays on the player
+            cooldown = 30000, -- Time in ms until the state starts to decay
         }
     },
 }
